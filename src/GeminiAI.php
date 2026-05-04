0488be2a35cb48a92ac3a7f16c0085a7f8910377
Xuan Thuy <xuanthuy01012001@gmail.com>
2026-05-04 16:16:23 +0700

    update model

--- src/GeminiAI.php
+++ src/GeminiAI.php
@@ -3,7 +3,7 @@
 class GeminiAI
 {
     private string $apiKey;
-    private string $endpoint = 'https://generativelanguage.googleapis.com/v1beta/models/imagen-4.0-fast-generate-001';
+    private string $endpoint = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent';
 
     public function __construct()
     {
